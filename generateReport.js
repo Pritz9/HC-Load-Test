2dc62fa52c8bd69e36e86a938d46aea116987c3e
Pritz9 <priti.pandey@techolution.com>
2025-07-17 23:36:53 +0530

    k6 Load Test Report Generate

--- generateReport.js
+++ generateReport.js
@@ -1,11 +1,16 @@
-// generateReport.js
 const fs = require('fs');
 const path = require('path');
 const reporter = require('k6-html-reporter');
 
+// Ensure 'reports' directory exists
+const reportsDir = path.join(__dirname, 'reports');
+if (!fs.existsSync(reportsDir)) {
+  fs.mkdirSync(reportsDir);
+}
+
 const options = {
-  jsonFile: 'result.json', // Your k6 summary output
-  output: path.join('reports', 'k6_report.html'), // Desired HTML output
+  jsonFile: 'result.json',
+  output: path.join(reportsDir, 'k6_report.html'),
 };
 
 reporter.generateSummaryReport(options);
