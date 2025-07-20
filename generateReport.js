cf5fc73227951a1c0cc06f3e6dd9be48fdae8273
Pritz9 <priti.pandey@techolution.com>
2025-07-20 18:22:11 +0530

    Load Test Email Report

--- generateReport.js
+++ generateReport.js
@@ -8,12 +8,12 @@ if (!fs.existsSync(reportsDir)) {
   fs.mkdirSync(reportsDir);
 }
 
-// Define full path to HTML file explicitly
+// Define full file path for output
 const outputFilePath = path.join(reportsDir, 'k6_report.html');
 
-// If a folder with that name exists from previous runs, remove it
+// Fix: If path already exists AND is a directory, delete it
 if (fs.existsSync(outputFilePath) && fs.lstatSync(outputFilePath).isDirectory()) {
-  fs.rmdirSync(outputFilePath, { recursive: true });
+  fs.rmSync(outputFilePath, { recursive: true, force: true });
 }
 
 const options = {
